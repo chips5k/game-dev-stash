5665622dc4f4fc38e3d502b2bfd7802443e57856
Callem Pittard <callem.pittard@gmail.com>
2016-11-02 22:24:50 +1100

    WIP edge/point correction

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 Repo to house simple game engine + basic games (primarily for learning/trying out new things)
+Trying to implement game engine code using functional programming techniques (ie immutable objects) as a means to learn FP too.
+I am completely aware of how inefficient this may be...
 
 References/Reading material:
 
 - http://www.gamedev.net/page/resources/_/technical/math-and-physics/a-verlet-based-approach-for-2d-game-physics-r2714
 - http://gafferongames.com/game-physics/fix-your-timestep/
 - http://gafferongames.com/game-physics/integration-basics/
+- http://www.gotoandplay.it/_articles/2005/08/advCharPhysics.php
--- src/main.ts
+++ src/main.ts
@@ -6,41 +6,36 @@ class RigidBody {
 }
 
 class BoundingBox {
-    readonly offsetCenter: Vector2d;
-    readonly edgeLengths: number[];
-    constructor(readonly points: Vector2d[]) {
-        let edges = [[0, 1], [1, 2], [2, 3], [3, 0]];
-        this.edgeLengths = [];
-
-        for(var i = 0; i < edges.length; i++) {
-            let oDiffVector = Vector2d.subtract(points[edges[i][0]], points[edges[i][1]]);
-            this.edgeLengths.push(Vector2d.magnitude(oDiffVector));
-        }
-
-    }
-
+    constructor(readonly points: Vector2d[], readonly width: number, readonly height: number) {}
 }
+
+
 
 class GameState {
     constructor(readonly rigidBodies: RigidBody[]){}
 }
 
-
-
 function advanceState(timestep: number, state: GameState, previousState: GameState) {
     let rigidBodies = state.rigidBodies.map((r: RigidBody, i: number) => {
 
         //Local forces (gravity)
-        let a = new Vector2d(0, r.mass * 0.0098);
-        let oPoints = r.boundingBox.points;
+        let a = new Vector2d(0, r.mass * 0.00098);
+
+        //recreate each point of the bounding box
         let nPoints = r.boundingBox.points.map((p: Vector2d, j: number) => {
-          
-            let currentVelocity = Vector2d.subtract(p, previousState.rigidBodies[i].boundingBox.points[j]);
             
-            let newVelocity = Vector2d.add(currentVelocity, Vector2d.multiply(a, timestep));
+            //By applying position verlet integration
 
+            //Calculate current velocity from previous two positions
+            let currentVelocity = Vector2d.subtract(p, previousState.rigidBodies[i].boundingBox.points[j]);
+
+            //Incorporate forces/accel over time
+            let newVelocity = Vector2d.add(currentVelocity, Vector2d.multiply(Vector2d.multiply(a, timestep), timestep));
+
+            //Update the position from the new velocity
             p = Vector2d.add(p, newVelocity);
-
+            
+            //Hack floor collision check
             if(p.y > 500) {
                 p = new Vector2d(p.x, p.y - (p.y - 500));
             }
@@ -48,22 +43,62 @@ function advanceState(timestep: number, state: GameState, previousState: GameSta
             return p;
         });
 
-        let edges = [[0, 1], [1, 2], [2, 3], [3, 0]];
+        //Research suggests i only need to constrain the 4 main edges, but in practice
+        //the box is able to be squashed whilst still validating the edge constraints.
+        //Thus the final two edges - "Diagonals" are required to enforce the distance between opposing points
+        //Note this behaviour is probably caused by dodgy collision detection with the floor.
+        let edges = [[0, 1], [1, 2], [2, 3], [3, 0], [0, 2], [1, 3]];
         
-       
+        //All of this needs to be refactored into something more managable but without relying
+        //on mutable state.
+        //Note, th
+        
+        //The following corrects edges and ensures
+        //our points/edges are constrained to the correct shape.
         edges.forEach((e, i) => {
-            
+
+            //Create a vector to represent difference between two points in an edge
             let nDiffVector = Vector2d.subtract(nPoints[e[1]], nPoints[e[0]]);
-            let diff = Vector2d.magnitude(nDiffVector) - r.boundingBox.edgeLengths[i];
-            
+
+            //Depending on which edge we are working with
+            //determine the "rest" length of the edge
+            //in our case its either the boxes height or width
+            var oLength = 0;
+            switch(i) {
+                case 0:
+                case 2:
+                    oLength = r.boundingBox.height;
+                break;
+
+                case 1:
+                case 3:
+                    oLength = r.boundingBox.width;
+                break;
+
+                case 4:
+                case 5:
+                    //For diagonals, we need the distance between two opposing corners
+                    let t = new Vector2d(r.boundingBox.width, r.boundingBox.height);
+                    oLength = Vector2d.magnitude(t);
+
+                break;
+
+            }
+
+            //Determine how deformed the edge is by calculating the diff from the rest length
+            let diff = Vector2d.magnitude(nDiffVector) - oLength;
+
+            //Generate a normalized vector to handle orienting our points
             let normal = Vector2d.normalize(nDiffVector);
-            
+
+            //add/subtract half the difference to each point in the edge, projecting along the normal
+            //obtained previously.
             nPoints[e[0]] = Vector2d.add(nPoints[e[0]], Vector2d.multiply(normal, diff/2));
             nPoints[e[1]] = Vector2d.subtract(nPoints[e[1]], Vector2d.multiply(normal, diff/2));
+        });
+        
 
-        }); 
-       
-        return new RigidBody(new BoundingBox(nPoints), r.mass);
+        return new RigidBody(new BoundingBox(nPoints, r.boundingBox.width, r.boundingBox.height), r.mass);
     });
 
     return new GameState(rigidBodies);
@@ -90,8 +125,8 @@ function renderState(state: GameState, canvas: HTMLCanvasElement, ctx: CanvasRen
 
 function main(window: Window, canvas: HTMLCanvasElement) {
     let ctx = canvas.getContext('2d');
-    let box = new BoundingBox([new Vector2d(10, 10), new Vector2d(10, 70), new Vector2d(50, 60), new Vector2d(50, 10)]);
-    let rigidBody = new RigidBody(box, 10);
+    let box = new BoundingBox([new Vector2d(10, 10), new Vector2d(10, 80), new Vector2d(50, 80), new Vector2d(50, 10)], 40, 70);
+    let rigidBody = new RigidBody(box,5);
     let state = new GameState([rigidBody]); 
     let previousState = state;
     let accumulated = 0;
