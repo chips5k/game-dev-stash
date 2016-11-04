d33c16b483381959455cc2417309ef9667cbf0a5
Callem Pittard <callem.pittard@gmail.com>
2016-11-04 21:58:42 +1100

    cleanup + fix bugs/refactor

--- src/main.ts
+++ src/main.ts
@@ -22,59 +22,45 @@ class GameState {
     constructor(readonly rigidBodies: RigidBody[]){}
 }
 
+function calculateVelocity(positionA: Vector2d, positionB: Vector2d) {
+    return Vector2d.subtract(positionA, positionB);
+}
+
+function combineForces(forces: Vector2d[]) {
+    return forces.reduce(Vector2d.add, new Vector2d(0, 0));
+}
+
+function integrateVelocity(velocity: Vector2d, acceleration: Vector2d, timestep: number) {
+    return Vector2d.add(velocity, Vector2d.multiply(acceleration, Math.pow(timestep, 2)));
+}
+
+function integratePosition(position: Vector2d, velocity: Vector2d, acceleration: Vector2d, timestep: number) {
+     return Vector2d.add(position, integrateVelocity(velocity, acceleration, timestep));
+}
+
 function advanceState(timestep: number, state: GameState, previousState: GameState) {
+    
     let rigidBodies = state.rigidBodies.map((r: RigidBody, i: number) => {
 
-        //Local forces (gravity)
-        let a = new Vector2d(0, r.mass * 0.00098);
-
-        //recreate each point of the bounding box
+        let acceleration = combineForces([new Vector2d(0, r.mass * 0.00098)]);
         let nPoints = r.path.points.map((p: Vector2d, j: number) => {
-            
-            //By applying position verlet integration
-
-            //Calculate current velocity from previous two positions
-            let currentVelocity = Vector2d.subtract(p, previousState.rigidBodies[i].path.points[j]);
-
-            //Incorporate forces/accel over time
-            let newVelocity = Vector2d.add(currentVelocity, Vector2d.multiply(Vector2d.multiply(a, timestep), timestep));
-
-            //Update the position from the new velocity
-            p = Vector2d.add(p, newVelocity);
-            
-            //Hack floor collision check
+            return integratePosition(p, calculateVelocity(p, previousState.rigidBodies[i].path.points[j]), acceleration, timestep);
+        }).map((p: Vector2d, j: number) => {
             if(p.y > 500) {
-                p = new Vector2d(p.x, p.y - (p.y - 500));
+                return new Vector2d(p.x, 500);
+            } else {
+                return p;
             }
-
-            return p;
         });
 
-        //The following corrects edges and ensures
-        //our points/edges are constrained to the correct shape.
         r.constraints.forEach((c, i) => {
-
-            //Create a vector to represent difference between two points in an edge
             let nDiffVector = Vector2d.subtract(nPoints[c.pathIndexB], nPoints[c.pathIndexA]);
-
-            //Depending on which edge we are working with
-            //determine the "rest" length of the edge
-            //in our case its either the boxes height or width
-            
-
-            //Determine how deformed the edge is by calculating the diff from the rest length
             let diff = Vector2d.magnitude(nDiffVector) - c.length;
-
-            //Generate a normalized vector to handle orienting our points
             let normal = Vector2d.normalize(nDiffVector);
-
-            //add/subtract half the difference to each point in the edge, projecting along the normal
-            //obtained previously.
             nPoints[c.pathIndexA] = Vector2d.add(nPoints[c.pathIndexA], Vector2d.multiply(normal, diff/2));
             nPoints[c.pathIndexB] = Vector2d.subtract(nPoints[c.pathIndexB], Vector2d.multiply(normal, diff/2));
         });
         
-
         return new RigidBody(new Path(nPoints, r.path.closed), r.constraints, r.mass);
     });
 
@@ -88,7 +74,7 @@ function renderState(state: GameState, canvas: HTMLCanvasElement, ctx: CanvasRen
         let points = state.rigidBodies[i].path.points;   
 
         ctx.beginPath();
-        ctx.moveTo(points[0].x, points[1].y);
+        ctx.moveTo(points[0].x, points[0].y);
         for(var j = 0; j < points.length; j++) {
             let p = points[j];
             ctx.lineTo(p.x, p.y);
@@ -104,29 +90,37 @@ function renderState(state: GameState, canvas: HTMLCanvasElement, ctx: CanvasRen
 }
 
 
+function createSquareRigidBody(width: number, height: number, position: Vector2d, mass: number) {
+    
+    let path = new Path([
+        position, 
+        new Vector2d(position.x, position.y + height), 
+        new Vector2d(position.x + width, position.y + height), 
+        new Vector2d(position.x + width, position.y)], 
+        true
+    );
 
-function main(window: Window, canvas: HTMLCanvasElement) {
-    let ctx = canvas.getContext('2d');
-
-    let width = 40;
-    let height = 70;
-
-    let path = new Path([new Vector2d(10, 10), new Vector2d(10, 80), new Vector2d(50, 80), new Vector2d(50, 10)], true);
-    let dimensions = new Vector2d(width, height);
-    let dimensionsLength = Vector2d.magnitude(dimensions);
     let constraints = [
         new Constraint(0, 1, height),
         new Constraint(1, 2, width),
         new Constraint(2, 3, height),
         new Constraint(3, 0, width),
-        new Constraint(0, 3, dimensionsLength),
-        new Constraint(2, 0, dimensionsLength)
+        new Constraint(1, 3, Vector2d.magnitude(new Vector2d(width, height)))
     ];
 
+    return new RigidBody(path, constraints, mass);
+}   
 
-    let rigidBody = new RigidBody(path, constraints, 1);
 
-    let state = new GameState([rigidBody]); 
+function main(window: Window, canvas: HTMLCanvasElement) {
+
+    let ctx = canvas.getContext('2d');
+
+    let state = new GameState([
+        createSquareRigidBody(40, 60, new Vector2d(250, 30), 2),
+        createSquareRigidBody(70, 20, new Vector2d(400, 10), 1)
+    ]); 
+
     let previousState = state;
     let accumulated = 0;
     let step = 1000/60;
@@ -147,11 +141,7 @@ function main(window: Window, canvas: HTMLCanvasElement) {
     };
 
     iterate(window.performance.now());
-
-
-    
 }
-
 
 main(window, <HTMLCanvasElement>document.getElementById('canvas'));
 
